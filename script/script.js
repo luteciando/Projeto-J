2dcb8ef27672af18ad08f6e72485a976b21eac0f
Otavio <otaviolutecio@gmail.com>
2026-03-10 20:30:00 -0300

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,45 @@
+const spider = document.getElementById("spiderCursor")
+
+document.addEventListener("mousemove",(e)=>{
+
+spider.style.left = e.clientX + "px"
+spider.style.top = e.clientY + "px"
+
+})
+
+
+
+window.goPage = function(page){
+
+setTimeout(()=>{
+
+document.querySelectorAll(".page").forEach(section=>{
+section.classList.remove("active")
+})
+
+document.getElementById(page).classList.add("active")
+
+},300)
+
+
+}
+
+
+
+document.addEventListener("click",()=>{
+
+spider.style.transform="scale(1.4)"
+
+setTimeout(()=>{
+spider.style.transform="scale(1)"
+},150)
+
+})
+
+
+
+setInterval(()=>{
+
+spider.style.transform="rotate("+(Math.random()*8-4)+"deg)"
+
+},1000)
